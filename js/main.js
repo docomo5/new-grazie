8904e7125c5b55130361a04f842699c17a8846d2
docomo5 <97150666+docomo5@users.noreply.github.com>
2022-03-23 16:11:46 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,5 +18,47 @@ $(function(){
     $('.container').mouseleave(function(){
       $('.sitemap-modal').removeClass('on');
     });
+
+    let num = 0;
+    let num2 = 0;
+
+    $('.btn-group1 .next').on('click', function(){
+        num++;
+        if(num > 3) {
+            return
+        }
+        $('.gallery1  img').removeClass('on');
+        $('.gallery1  img').eq(num).addClass('on');
+    })
+
+
+    $('.btn-group1 .prev').on('click', function(){
+        num--;
+        if(num < 0) {
+            return
+        }
+        $('.gallery1  img').removeClass('on');
+        $('.gallery1  img').eq(num).addClass('on');
+    })
+
+    
+    $('.btn-group2 .next').on('click', function(){
+        num2++;
+        if(num2 > 3) {
+            return
+        }
+        $('.gallery2  img').removeClass('on');
+        $('.gallery2  img').eq(num2).addClass('on');
+    })
+
+
+    $('.btn-group2 .prev').on('click', function(){
+        num2--;
+        if(num2 < 0) {
+            return
+        }
+        $('.gallery2  img').removeClass('on');
+        $('.gallery2  img').eq(num2).addClass('on');
+    })
   
   })
